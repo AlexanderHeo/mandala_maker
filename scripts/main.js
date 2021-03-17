f57b9599ac0b0a3b5bacd77062d227a079bdcccd
Alexander Heo <jikster712@gmail.com>
2021-03-16 18:33:18 -0700

    eraser function - checkbox uncheck line, can choose eraser size

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let canvas, context, w, h,
-	prevX = 0, currX = 0, prevY = 0, currY = 0,	draw = false
+	prevX = 0, currX = 0, prevY = 0, currY = 0,	draw = false, fill = false
 
 function init() {
 	canvas = document.querySelector("canvas")
@@ -22,6 +22,24 @@ const addClickListeners = () => {
 	document.querySelector("#clear").onclick = clearCanvas
 	document.querySelector('#menu-close-button').onclick = toggleMenu
 	document.querySelector('#background').onchange = handleBackgroundChange
+	document.querySelector('#eraser').onchange = handleCheckboxChange
+	document.querySelector('#lines').onchange = handleCheckboxChange
+}
+
+const handleCheckboxChange = e => {
+	const name = e.target.name
+	console.log(e.target.name)
+
+
+	if (name === 'eraser') {
+		const lines = document.querySelector('#lines')
+		if (lines.checked) lines.checked = false
+	}
+
+	if (name === 'lines') {
+		const eraser = document.querySelector('#eraser')
+		if (eraser.checked) eraser.checked = false
+	}
 }
 
 const toggleMenu = () => {
@@ -47,14 +65,25 @@ const handlePointerDown = e => {
 	draw = true
 }
 
+const handleFillSection = e => {
+	recordPointerLocation(e)
+	fillSection()
+}
+
 const drawLine = () => {
 	var a = prevX,
 	b = prevY,
 	c = currX,
 	d = currY
 
-	context.strokeStyle = getColor()
-	context.lineWidth = getWidth()
+	if (document.querySelector('#eraser').checked) {
+		context.strokeStyle = getBackground()
+		context.lineWidth = getEraserWidth()
+	} else {
+		context.strokeStyle = getColor()
+		context.lineWidth = getWidth()
+	}
+
 	context.lineCap = "round"
 
 	context.beginPath()
@@ -64,6 +93,10 @@ const drawLine = () => {
 
 	context.stroke()
 	context.closePath()
+}
+
+const fillSection = () => {
+	console.log(prevX, prevY, currX, currY)
 }
 
 const stopDrawing = () => {
@@ -77,23 +110,24 @@ const clearCanvas = () => {
 }
 
 const getColor = () => {
-	return document.querySelector("#line").value
-	// return '#000000'
+	return document.querySelector("#color").value
 }
 
 const getWidth = () => {
 	return document.querySelector("#width").value
-	// return 1
+}
+
+const getEraserWidth = () => {
+	return document.querySelector("#eraserSize").value
+	console.log(document.querySelector("#eraserSize").value)
 }
 
 const getFill = () => {
 	return document.querySelector("#fill").value
-	// return '#c0de25'
 }
 
 const getBackground = () => {
 	return document.querySelector("#background").value
-	// return '#e1e1e1'
 }
 
 const handleBackgroundChange = () => {
