1f05ec5efbea2d743a72a9e56fc596e5892dbf2d
Alexander Heo <jikster712@gmail.com>
2021-12-14 17:11:09 -0800

    feat: logic to pick eraser size, choose between line or eraser

--- scripts/script.js
+++ scripts/script.js
@@ -44,13 +44,39 @@ const addListeners = () => {
   canvas.onpointerout = stopDrawing;
   canvas.style.cursor = 'crosshair';
 
+  const buttons = document.querySelectorAll('.button');
   const colors = document.querySelectorAll('.line-color');
   const colorWell = document.querySelector('#current-color');
   const lineSize = document.querySelectorAll('.line-size');
+  const erasers = document.querySelectorAll('.eraser-button');
 
+  buttons.forEach((el) => el.addEventListener('click', handleButtonClick));
   colors.forEach((el) => el.addEventListener('click', handleColorChange));
   colorWell.addEventListener('change', handleColorWellChange);
   lineSize.forEach((el) => el.addEventListener('click', handleLineWidth));
+  erasers.forEach((el) => el.addEventListener('click', handleEraser));
+};
+
+const handleButtonClick = (e) => {
+  const target = e.currentTarget;
+  const name = target.name;
+  if (name === 'line' || name === 'eraser') {
+    handleToolButton(e);
+  }
+};
+
+const handleToolButton = (e) => {
+  const target = e.currentTarget;
+  const name = target.name;
+  const line = document.querySelector('.line-picker-button');
+  const eraser = document.querySelector('.eraser-picker-button');
+  if (name === 'line') {
+    line.classList.add('active');
+    eraser.classList.remove('active');
+  } else if (name === 'eraser') {
+    eraser.classList.add('active');
+    line.classList.remove('active');
+  }
 };
 
 const handleColorChange = (e) => {
@@ -60,6 +86,9 @@ const handleColorChange = (e) => {
   const el = document.querySelector('.color.active');
   const colorEl = document.querySelector(`.color.${name}`);
   const currentColor = document.querySelector('#current-color');
+  const line = document.querySelector('.line-picker-button');
+  const eraser = document.querySelector('.eraser-picker-button');
+
   if (wellOrButton === 'well') {
     wellOrButton = 'button';
   } else if (wellOrButton === 'button') {
@@ -67,17 +96,23 @@ const handleColorChange = (e) => {
     colorEl.classList.add('active');
   }
   currentColor.value = value;
+  line.classList.add('active');
+  eraser.classList.remove('active');
 };
 
 const handleColorWellChange = (e) => {
   const target = e.currentTarget;
   const value = target.value;
   const colorButton = document.querySelector('.line-color.active');
+  const line = document.querySelector('.line-picker-button');
+  const eraser = document.querySelector('.eraser-picker-button');
 
   if (wellOrButton === 'button') {
     colorButton.classList.remove('active');
   }
   wellOrButton = 'well';
+  line.classList.add('active');
+  eraser.classList.remove('active');
 };
 
 const handleLineWidth = (e) => {
@@ -91,4 +126,18 @@ const handleLineWidth = (e) => {
   clickedEl.classList.add('active');
   currentSizeEl.classList.remove(currentSizeEl.classList.item(1));
   currentSizeEl.classList.add(`line-${size}`);
+};
+
+const handleEraser = (e) => {
+  const target = e.currentTarget;
+  const name = target.name;
+  const activeEl = document.querySelector('.eraser-button.active');
+  const clickedEl = document.querySelector(`.eraser-button.${name}`);
+  const line = document.querySelector('.line-picker-button');
+  const eraser = document.querySelector('.eraser-picker-button');
+
+  activeEl.classList.remove('active');
+  clickedEl.classList.add('active');
+  line.classList.remove('active');
+  eraser.classList.add('active');
 };
