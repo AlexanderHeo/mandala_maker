e5386bc241ed07d177360761adc8912a1a2d84fe
Alexander Heo <jikster712@gmail.com>
2021-12-14 20:37:59 -0800

    feat: logic minor refactors

--- scripts/background.js
+++ scripts/background.js
@@ -3,14 +3,14 @@
 const handleBackgroundChange = () => {
   const bgc = getBackground();
   const lineShade = lightOrDark(bgc);
-  const overlay = document.querySelector(".overlay");
+  const overlay = document.querySelector('.overlay');
 
-  if (lineShade === "light") {
-    overlay.classList.remove("dark");
-    overlay.classList.add("light");
-  } else if (lineShade === "dark") {
-    overlay.classList.remove("light");
-    overlay.classList.add("dark");
+  if (lineShade === 'light') {
+    overlay.classList.remove('dark');
+    overlay.classList.add('light');
+  } else if (lineShade === 'dark') {
+    overlay.classList.remove('light');
+    overlay.classList.add('dark');
   }
 
   context.fillStyle = bgc;
@@ -20,7 +20,7 @@ const handleBackgroundChange = () => {
 const lightOrDark = (color) => {
   var r, g, b, hsp;
 
-  color = +("0x" + color.slice(1).replace(color.length < 5 && /./g, "$&$&"));
+  color = +('0x' + color.slice(1).replace(color.length < 5 && /./g, '$&$&'));
   r = color >> 16;
   g = (color >> 8) & 255;
   b = color & 255;
@@ -28,6 +28,6 @@ const lightOrDark = (color) => {
   // http://alienryderflex.com/hsp.html
   hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
 
-  if (hsp > 127.5) return "dark";
-  else return "light";
+  if (hsp > 127.5) return 'dark';
+  else return 'light';
 };
