b10567d1b8da7b11b36b3d5c7b94e71c0b7f9775
Alexander Heo <jikster712@gmail.com>
2021-12-14 20:37:03 -0800

    feat: refactor get logics

--- scripts/get.js
+++ scripts/get.js
@@ -1,12 +1,55 @@
 /* eslint-disable no-unused-vars */
-const getColor = () => document.querySelector("#color").value;
+const getColor = () => {
+  return document.querySelector('#current-color').value;
+};
 
-const getBackground = () => document.querySelector("#background").value;
+const getWidth = () => {
+  const el = document.querySelector('#current-line');
+  const val = el.classList[1];
+  switch (val) {
+    case 'line-xsmall':
+      return 1;
+    case 'line-small':
+      return 3;
+    case 'line-medium':
+      return 6;
+    case 'line-regular':
+      return 9;
+    case 'line-large':
+      return 15;
+    case 'line-xlarge':
+      return 20;
+    default:
+      return 1;
+  }
+};
 
-const getFill = () => document.querySelector("#fillColor").value;
+const getEraserSize = () => {
+  const el = document.querySelector('.eraser-button.active');
+  const val = el.classList[1];
+  switch (val) {
+    case 'xsmall':
+      return 1;
+    case 'small':
+      return 3;
+    case 'medium':
+      return 6;
+    case 'regular':
+      return 9;
+    case 'large':
+      return 15;
+    case 'xlarge':
+      return 20;
+    default:
+      return 1;
+  }
+};
 
-const getWidth = () => document.querySelector("#width").value;
+const getBackground = () => {
+  const el = document.querySelector('.background-color.active');
+  return el.classList[3];
+};
 
-const getSlices = () => document.querySelector("#pointsSize").value;
-
-const getEraserSize = () => document.querySelector("#eraserSize").value;
+const getSlices = () => {
+  return document.querySelector('#pointsSize').value;
+};
